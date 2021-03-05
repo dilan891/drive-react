33835d513e1204a4573451eda2f2e4d8c042f89b
dilan891 <dilandeornelas@gmail.com>
2021-03-05 17:06:40 -0400

    peticion fecth para carpetas

--- src/Todos.js
+++ src/Todos.js
@@ -1,20 +1,33 @@
 import "./card.css"
-//import {useState,useEffect} from "react";
+import {useState,useEffect} from "react";
 
 export default function Todos(){
-    /*useEffect(()=>{
-        fetch("/")
-        .then(e => console.log(e))
-        .catch(e => console.log(e))
-    })*/
+    const [carpets,setcarpet] =  useState([]);
+    
+    function peticion(){    //peticion al servidor para obtener las carpetas 
+        fetch("http://localhost:4000/carpets")
+        .then(e => e.json())
+        .then(e => {
+            console.log(e)
+            setcarpet(e)
+        })
+        .catch(e => {
+            console.log(e)
+            setcarpet([{name: "carpet1"},{name: "carpet2"}]);
+        })
+    }
+    
+    useEffect(()=>{
+        peticion()
+    },[])  //matriz vacia para que no se ejecute un loop
+
     const imagenes = ["imagen1","imagen2","imagen3","imagen4","iamgen5","imagen6"]
-    const carpet = ["carpet1","carpet2"];
     return(
         <div className="card-content">
-            {carpet.map(c=>{
+            {carpets.map(c=>{ 
                 return(
                     <div key={c} className="card">
-                        {c}
+                        {c.name}
                     </div>
                 )
             })}
