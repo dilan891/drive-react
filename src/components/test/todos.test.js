50a3566ba4408ccf896c4d3b3c88e7c89054cd2b
dilan891 <dilandeornelas@gmail.com>
2021-04-07 12:17:46 -0400

    test config

--- src/components/test/todos.test.js
+++ src/components/test/todos.test.js
@@ -1,29 +1,30 @@
 import React from "react";
-import Todos from "../Todos";
+import Todos from "../Todos"
 import { BrowserRouter as Router } from 'react-router-dom';
-import {screen,render, waitFor} from "@testing-library/react";
+import { screen, render, waitFor } from "@testing-library/react";
 import renderer from "react-test-renderer";
 import { UseMenuSelect } from "../../context/useMenuSelect";
+import { UseToast } from "../../context/useToast"
 
 
-beforeEach(()=>render(<Router><UseMenuSelect><Todos setID={((e)=>{console.log(e)}).bind(this)} /></UseMenuSelect></Router> ))
-describe("todos menu",()=>{
-    it("fetch carpeta",async()=>{ 
-        await waitFor(()=>{screen.getByRole("heading")})
+beforeEach(() => render(<Router><UseMenuSelect><UseToast><Todos setID={((e) => { console.log(e) }).bind(this)} /></UseToast></UseMenuSelect></Router>))
+describe("todos menu", () => {
+    it("fetch carpeta", async () => {
+        await waitFor(() => { screen.getByRole("heading") })
         expect(screen.getByRole("heading")).toHaveTextContent("juan")
     });
-    it("fecth archivos",async()=>{
-        await waitFor(()=> screen.getByText(/archivo1/i))
+    it("fecth archivos", async () => {
+        await waitFor(() => screen.getByText(/archivo1/i))
         expect(screen.getByText(/archivo1/i)).toBeInTheDocument()
     });
-    it("render img",async()=>{
-        await waitFor(()=>{screen.getByRole("img")})
+    it("render img", async () => {
+        await waitFor(() => { screen.getByRole("img") })
         expect(screen.getByRole("img")).toBeInTheDocument()
     });
-    it("snapshot 1",async()=>{        
-        const Component = renderer.create(<Router><Todos setID={((e)=>{console.log(e)}).bind(this)} /></Router> ) 
+    it("snapshot 1", async () => {
+        const Component = renderer.create(<Router><UseMenuSelect><UseToast><Todos setID={((e) => { console.log(e) }).bind(this)} /></UseToast></UseMenuSelect></Router>)
         let tree = Component.toJSON();
-        
+
         expect(tree).toMatchSnapshot();
     });
 })
