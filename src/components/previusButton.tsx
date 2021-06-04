b78df3b9eeac9054ceafeb8c65000a00de904c93
dilan891 <dilandeornelas@gmail.com>
2021-06-04 10:26:21 -0400

    previus added

--- src/components/previusButton.tsx
+++ src/components/previusButton.tsx
@@ -3,19 +3,29 @@ import { Link } from "react-router-dom"
 import { ToastsContext } from '../context/useToast'
 
 const PreviusButton: React.FC = () => {
-    const { previus,updater } = useContext(ToastsContext)
+    const { previus, updater, previusDelete } = useContext(ToastsContext)
 
-    const updateDAta = ()=>{
-        updater()
+    const updateDAta = () => {
+        updater();
+        previusDelete();
     }
 
-    if (previus === null) {
+    const redirect: any = () => { //redirige a el menu todos en caso de ser necesario
+        if (previus[previus.length - 1] === "none") {
+            return "/Todos"
+        }
+        else {
+            return "/carpeta/" + previus[previus.length - 1]
+        }
+    }
+
+    if (previus.length === 0) {
         return <div></div>
     }
 
     else {
         return <div className="previus-button">
-            <Link onClick={updateDAta} to={"/carpeta/" + previus } >
+            <Link onClick={updateDAta} to={redirect} >
                 <button>
                     {"<-----"}
                 </button>
